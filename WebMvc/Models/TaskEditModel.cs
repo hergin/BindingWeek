2a37047413238ce01cab9518dcfd30fbceba3ef5
Huseyin Ergin <hergin@bsu.edu>
2024-02-12 23:11:48 -0500

    Update TaskEditModel.cs

--- WebMvc/Models/TaskEditModel.cs
+++ WebMvc/Models/TaskEditModel.cs
@@ -10,10 +10,10 @@ namespace WebMvc.Models
         public int Id { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        public string? Title { get; set; }
+        public string Title { get; set; }
         [Display(Name = "Task Content")]
-        public string? Content { get; set; }
+        public string Content { get; set; }
         [DataType(DataType.Date)]
-        public DateTime? DueDate { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
