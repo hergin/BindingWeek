c915b569c367f36d4ea5ace58e74133163492d74
yabetsg <yabets.g2@gmail.com>
2024-02-25 23:30:10 -0500

    Implement dependency injection

--- WebMvc/Service/TaskService.cs
+++ WebMvc/Service/TaskService.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DomainModel;
+
 namespace WebMvc.Service
 {
-    public class TaskService
+    public class TaskService : ITaskService
     {
         List<MyTask> tasks;
         public TaskService()
