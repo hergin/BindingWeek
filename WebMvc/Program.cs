bd8209ccc118e9dbe082193c57c444b72ab358b9
Tia Self <tiamarie.self@bsu.edu>
2024-02-26 19:30:08 -0500

    Dependency Injection

--- WebMvc/Program.cs
+++ WebMvc/Program.cs
@@ -1,3 +1,5 @@
+using WebMvc.Service;
+
 namespace WebMvc;
 
 public class Program
@@ -8,6 +10,8 @@ public class Program
 
         // Add services to the container.
         builder.Services.AddControllersWithViews();
+
+        builder.Services.AddSingleton<ITaskService, TaskService>();
 
         var app = builder.Build();
 
