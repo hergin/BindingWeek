992a77e19f884487d0ebca245c6543c5ff9490a9
Megan237 <megan.stinefield@bsu.edu>
2024-02-28 20:43:49 -0500

    Task service dependency injection

--- WebMvc/Controllers/HomeController.cs
+++ WebMvc/Controllers/HomeController.cs
@@ -10,22 +10,23 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public static TaskService taskService = new TaskService();
+    private readonly TaskService _taskService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, TaskService taskService)
     {
         _logger = logger;
+        _taskService = taskService;
     }
 
     public IActionResult Index()
     {
-        return View(taskService.GetAllTasks().Select(t => TaskViewModel.FromTask(t)));
+        return View(_taskService.GetAllTasks().Select(t => TaskViewModel.FromTask(t)));
     }
 
     // GET: /HelloWorld/Edit/{id}
     public IActionResult Edit([FromRoute] int id)
     {
-        var theTask = taskService.FindTaskByID(id);
+        var theTask = _taskService.FindTaskByID(id);
         var taskEditModel = TaskEditModel.FromTask(theTask);
         return View(taskEditModel);
     }
@@ -39,7 +40,7 @@ public class HomeController : Controller
     {
         if (ModelState.IsValid)
         {
-            taskService.UpdateTaskByID(id, task.Title, task.Content, task.DueDate);
+            _taskService.UpdateTaskByID(id, task.Title, task.Content, task.DueDate);
             return RedirectToAction("ViewTask", new { id = id });
         }
         else
@@ -50,7 +51,7 @@ public class HomeController : Controller
 
     public IActionResult ViewTask([FromRoute] int id)
     {
-        var theTask = taskService.FindTaskByID(id);
+        var theTask = _taskService.FindTaskByID(id);
         return View(TaskViewModel.FromTask(theTask));
     }
 
@@ -66,7 +67,7 @@ public class HomeController : Controller
     {
         if (ModelState.IsValid)
         {
-            taskService.CreateTask(task.Title, task.Content, task.DueDate);
+            _taskService.CreateTask(task.Title, task.Content, task.DueDate);
             return RedirectToAction("Index");
         }
         else
