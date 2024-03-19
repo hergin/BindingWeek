12ee639f64a444e32ee5e765101744d7025d2ce8
tsnicholas <tsnicholas@bsu.edu>
2024-03-19 13:32:28 -0400

    Add a database to replace the task list

---
+++ Database/DbConnection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DomainModel;
+namespace Database;
+
+public class DbConnection
+{
+    private MyTaskContext db;
+
+    public DbConnection()
+    {
+        db = new MyTaskContext();
+    }
+
+    public void Save(MyTask newTask)
+    {
+        db.Add(newTask);
+        db.SaveChanges();
+    }
+
+    public int Count() {
+        return db.Tasks.Count();
+    }
+
+    public List<MyTask> GetAllTasks() {
+        return db.Tasks.OrderBy(task => task.Id).ToList();
+    }
+
+    public MyTask ParseById(int id) {
+        return db.Tasks.Single(task => task.Id == id);
+    }
+
+    public void UpdateById(int id, string title, string content, DateTime dueDate) {
+        MyTask selectedTask = db.Tasks.Single(task => task.Id == id);
+        selectedTask.Update(title, content, dueDate);
+        db.SaveChanges();
+    }
+}
---
+++ Database/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var mainDirectory = Path.GetFullPath("..");
+Console.WriteLine(@$"{mainDirectory}\Database\tasks.db");
